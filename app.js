c4b170fec27420f2ffbee94ac86f728ab2c7c013
valts08 <valts.pilans@gmail.com>
2026-01-11 20:42:34 +0200

    added routes and implemented the user blogs route

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 const express = require('express')
 const app = express()
 
-const dummy = {
-    id: 1,
+const dummy = [{
+    blog_id: 1,
+    user_id: 1,
     time: Date.now()
-}
+},{
+    blog_id: 2,
+    user_id: 1,
+    time: Date.now()
+},{
+    blog_id: 3,
+    user_id: 1,
+    time: Date.now()
+}]
 
 app.use(express.json())
 
-app.get('/', (req, res, next) => {
+// get blogs by user id
+app.get('/api/blogs/user/:id', (req, res, next) => {
+    const userId = parseInt(req.params.id)
+    const blogs = dummy.filter(user => user.user_id === userId)
+
+    res.send({blogs})
+    console.log('GET user blogs')
+})
+
+// get blog by id
+app.get('/api/blog/:id', (req, res, next) => {
     res.send({dummy})
-    console.log('got GET')
+    console.log('GET blog by id')
+})
+
+// get all blogs
+app.get('/api/blogs', (req, res, next) => {
+    res.send({dummy})
+    console.log('GET all blogs')
 })
 
 app.listen(3000, 'localhost', () => console.log('connected'))
